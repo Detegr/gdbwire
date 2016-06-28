eeac9deba24a1278006fd9e67cc02e115d5bbc40
Antti Keränen <detegr@gmail.com>
2016-06-28 20:18:14 +0300

    Add license

--- examples/gdbmi_example.rs
+++ examples/gdbmi_example.rs
@@ -1,5 +1,13 @@
-extern crate gdbwire;
+// Copyright (c) 2015 gdbwire crate developers
+// Licensed under the Apache License, Version 2.0
+// <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT
+// license <LICENSE-MIT or http://opensource.org/licenses/MIT>,
+// at your option. All files in the project carrying such
+// notice may not be copied, modified, or distributed except
+// according to those terms.
 
+extern crate gdbwire;
 use gdbwire::*;
 
 fn main() {
